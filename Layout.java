39e815e53ca4872ea6edb4bcd5d96af31f9fc870
Diepolli <linus_diepold@web.de>
2015-01-28 19:25:25 +0100

    Penis

--- Layout.java
+++ Layout.java
@@ -2,18 +2,23 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class Layout {
+	private JPanel Feld;
+	//private JTextField Feld1;
+	JTextField[][] Reihen = new JTextField[9][9];
 	
 	public Layout()
 	{
 	JFrame window= new JFrame();
-	window.setTitle("Funktions-Plotter");
+	window.setTitle("Sudoku");
 	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	window.setSize(800,800);
 	window.setBackground(Color.lightGray);
 	
+	JPanel Feld=new JPanel();
 	
 	
 	window.setVisible(true);
