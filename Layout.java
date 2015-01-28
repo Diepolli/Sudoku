59f1a264b25d9e6044db589f66fdc7659895dd3a
Diepolli <linus_diepold@web.de>
2015-01-28 19:07:17 +0100

    Ein Fenster
    sehe und staune

--- Layout.java
+++ Layout.java
@@ -1,4 +1,27 @@
 
-public class Layout {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
 
+public class Layout {
+	
+	public Layout()
+	{
+	JFrame window= new JFrame();
+	window.setTitle("Funktions-Plotter");
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	window.setSize(800,800);
+	window.setBackground(Color.lightGray);
+	
+	
+	
+	window.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Layout Sudoku = new Layout();
+	}
 }
